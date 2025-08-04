d9a907e47cd0bd9708035341feeb761091ec499a
shivampan <shivampwork12012000@gmail.com>
2025-08-04 10:49:05 +0530

    Bean annotation has been added in the config class

--- src/main/java/com/shivam/ecommerce2025/ApplicationConfiguration/ApplicationConfig.java
+++ src/main/java/com/shivam/ecommerce2025/ApplicationConfiguration/ApplicationConfig.java
@@ -1,8 +1,12 @@
 package com.shivam.ecommerce2025.ApplicationConfiguration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+@Configuration
 public class ApplicationConfig {
+    @Bean
     public RestTemplate createRestTemplateBean(){
         return new RestTemplate();
 
