0e7402bfbf4432450ce2304937c372e5a9d40f8f
shivampan <shivampwork12012000@gmail.com>
2025-08-06 13:39:39 +0530

    The ExceptionDto has been added

--- src/main/java/com/shivam/ecommerce2025/controllerAdvise/ProductServiceExceptionHandler.java
+++ src/main/java/com/shivam/ecommerce2025/controllerAdvise/ProductServiceExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.shivam.ecommerce2025.controllerAdvise;
 
 
+import com.shivam.ecommerce2025.dtos.ExceptionDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,7 +10,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ProductServiceExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<Void> runTimeException(){
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    public ResponseEntity<ExceptionDto> runTimeException(){
+
+        ExceptionDto exceptionDto = new ExceptionDto();
+        exceptionDto.setMessage("Something went wrong");
+        exceptionDto.setResolutionDetails("You need to pay more money to get this fixed from us.Thanks!!");
+
+        return new ResponseEntity<>(
+                exceptionDto,
+                HttpStatus.NOT_FOUND);
     }
 }
---
+++ src/main/java/com/shivam/ecommerce2025/dtos/ExceptionDto.java
@@ -0,0 +1,14 @@
+package com.shivam.ecommerce2025.dtos;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ExceptionDto {
+
+    private String message;
+    private String resolutionDetails;
+
+}
